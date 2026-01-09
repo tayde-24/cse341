3fc7d110593ee156cd719fd7a97ca7024952ef79
Tayde Luevano <tayde_luevano@yahoo.com>
2026-01-08 20:57:17 -0800

    nodemon works

---
+++ server.js
@@ -0,0 +1,17 @@
+//express web server
+/*var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;*/
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hanna Birch');
+});
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
