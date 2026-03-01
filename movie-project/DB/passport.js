747e92d7a0e0ea6f8f31aba864e4d25c3cda564a
Tayde Luevano <tayde_luevano@yahoo.com>
2026-02-28 21:20:53 -0800

    Added mongoose

--- movie-project/DB/passport.js
+++ movie-project/DB/passport.js
@@ -1,4 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = require('../models/User');
 
 module.exports = function(passport) {
     passport.use(new GoogleStrategy({
@@ -7,32 +9,35 @@ module.exports = function(passport) {
         callbackURL: '/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        // const newUser = {
-        //     googleId: profile.id,
-        //     displayName: profile.displayName,
-        //     firstName: profile.name.givenName,
-        //     lastName: profile.name.familyName,
-        //     image: profile.photos[0].value
-        // }
-        // try {
-        //     let user = await User.findOne({googleId: profile.id})
-        //     if (user) {
-        //         done(null,user)
-        //     } else {
-        //         user = await User.create(newUser)
-        //         done(null, user)
-        //     } 
-        // } catch(err) {
-        //     console.error(err);
-        // }
+        //console.log(profile);
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+        try {
+            let user = await User.find({googleId: profile.id})
+            if (user) {
+                done(null,user);
+            } else {
+                user = await User.create(newUser);
+                done(null, user);
+            } 
+        } catch(err) {
+            console.error(err);
+        }
     }))
 
     passport.serializeUser((user, done) => {
+        console.log("Inside here");
         done(null, user.id);
     })
 
     passport.deserializeUser((id,done) => {
         //See what I need to put down here
+        /**/ 
+        User.findById(id, (err, user) => done(err, user));
     })
 }
