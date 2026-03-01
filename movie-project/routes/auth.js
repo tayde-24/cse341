a47b77611d20b84b4f82108f11c8ea78b0da82f2
Tayde Luevano <tayde_luevano@yahoo.com>
2026-02-28 21:20:05 -0800

    Added logout router

--- movie-project/routes/auth.js
+++ movie-project/routes/auth.js
@@ -18,6 +18,12 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
 (req, res) => {
     res.redirect('/dashboard')
 }
-)
+);
 
+// @desc Logout user
+// @route /auth/logout
+router.get('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
+})
 module.exports = router;
